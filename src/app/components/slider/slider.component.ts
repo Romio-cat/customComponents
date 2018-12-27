96364347b065ce130178b8f9788b04c99f794220
Romio-cat <kaluzhenov95@gmail.com>
2018-12-27 17:44:46 +0200

    added navigation for slider with animation

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,72 +1,101 @@
-import {Component, OnChanges, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent implements OnInit, OnChanges {
+export class SliderComponent implements OnInit {
   public sliders: string[] = [
     'first slider',
     'second slider',
     'third slider',
     'forth slider',
   ];
-  public counter = 0;
+  private width = 600;
+  public current = 0;
   public total = this.sliders.length;
   public currentTransition: number[] = [];
 
   constructor() { }
-
-  ngOnChanges() {
-  }
 
   ngOnInit() {
     for (let i = 0; i < this.total; i++) {
       this.currentTransition[i] = 0;
     }
 
-    this.currentTransition[this.total - 1] = -500 * (this.total);
-
-    // console.log(this.currentTransition);
+    this.currentTransition[this.total - 1] = -this.width * this.total;
   }
 
   public inc(): void {
-    let outerIndex = this.counter - 1;
-    this.counter = (this.counter < this.total - 1) ? this.counter + 1 : 0;
+    let moved = this.current - 1;
+    this.current = (this.current < this.total - 1) ? this.current + 1 : 0;
 
-    outerIndex = outerIndex < 0 ? this.total - 1 : outerIndex;
+    moved = moved < 0 ? this.total - 1 : moved;
 
     for (let i = 0; i < this.total; i++) {
-      this.currentTransition[i] = this.currentTransition[i] - 500;
+      this.currentTransition[i] = this.currentTransition[i] - this.width;
     }
 
-    this.currentTransition[outerIndex] = this.currentTransition[outerIndex] + 500 * this.total;
-    // console.log('outerIndex =  ', outerIndex);
-    // console.log('counter =  ', this.counter);
+    this.currentTransition[moved] = this.currentTransition[moved] + this.width * this.total;
+
+    // console.log('current =  ', this.current);
+    // console.log('moved =  ', moved);
     // console.log(this.currentTransition);
   }
 
   public dec(): void {
-    this.counter = (this.counter > 0 ) ? this.counter - 1 : this.total - 1;
-    let outerIndex = this.counter - 1;
+    this.current = (this.current > 0 ) ? this.current - 1 : this.total - 1;
+    let moved = this.current - 1;
 
-    outerIndex = (outerIndex < 0) ? this.total - 1 : outerIndex;
+    moved = (moved < 0) ? this.total - 1 : moved;
 
     for (let i = 0; i < this.total; i++) {
-      this.currentTransition[i] = this.currentTransition[i] + 500;
+      this.currentTransition[i] = this.currentTransition[i] + this.width;
     }
 
-    this.currentTransition[outerIndex] = this.currentTransition[outerIndex] - 500 * this.total;
-    // console.log('count = ' + this.counter);
-    // console.log('outerIndex = ' + outerIndex);
+    this.currentTransition[moved] = this.currentTransition[moved] - this.width * this.total;
+
+    // console.log('current = ' + this.current);
+    // console.log('moved = ' + moved);
     // console.log(this.currentTransition);
   }
 
   public showSlide(index) {
-    this.counter = index;
+    let diff = index - this.current;
+    let moved;
+
+    if (diff > 0) {
+      for (let i = 0; i < diff; i++) {
+        moved = this.current - 1;
+        this.current++;
+        this.current = (this.current > this.total - 1) ? 0 : this.current;
+
+        moved = moved < 0 ? this.total - 1 : moved;
+
+        for (let j = 0; j < this.total; j++) {
+          this.currentTransition[j] = this.currentTransition[j] - this.width;
+        }
+
+        this.currentTransition[moved] = this.currentTransition[moved] + this.width * this.total;
+      }
+    } else {
+      diff = Math.abs(diff);
+
+      for (let i = 0; i < diff; i++) {
+        this.current--;
+        this.current = this.current < 0 ? 0 : this.current;
+        moved = this.current - 1;
+
+        moved = (moved < 0) ? this.total - 1 : moved;
+
+        for (let j = 0; j < this.total; j++) {
+          this.currentTransition[j] = this.currentTransition[j] + this.width;
+        }
+
+        this.currentTransition[moved] = this.currentTransition[moved] - this.width * this.total;
+      }
+    }
+
   }
-
-
-
 }
