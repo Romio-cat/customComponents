5e1ed2cc197531738f2e8ef809007706cf082b20
Romio-cat <kaluzhenov95@gmail.com>
2018-12-27 12:30:06 +0200

    added the ability to scroll slider with animation

--- src/app/components/smart/slider/slider.component.ts
+++ src/app/components/smart/slider/slider.component.ts
@@ -22,17 +22,13 @@ export class SliderComponent implements OnInit, OnChanges {
   }
 
   ngOnInit() {
-    // console.log('% ' + this.counter % this.total);
-    // console.log('count ' + this.counter);
-    // console.log('amount ' + this.amount);
-
     for (let i = 0; i < this.total; i++) {
       this.currentTransition[i] = 0;
     }
 
     this.currentTransition[this.total - 1] = -500 * (this.total);
 
-    console.log(this.currentTransition);
+    // console.log(this.currentTransition);
   }
 
   public inc(): void {
@@ -44,32 +40,27 @@ export class SliderComponent implements OnInit, OnChanges {
     for (let i = 0; i < this.total; i++) {
       this.currentTransition[i] = this.currentTransition[i] - 500;
     }
-    console.log('outerIndex =  ', outerIndex);
-    console.log('counter =  ', this.counter);
-    this.currentTransition[outerIndex] = this.currentTransition[outerIndex] + 500 * (this.total);
 
-    console.log(this.currentTransition);
-
-    // console.log('% ' + this.counter % this.total);
-    // console.log('count ' + this.counter);
-    // console.log('amount ' + this.amount);
+    this.currentTransition[outerIndex] = this.currentTransition[outerIndex] + 500 * this.total;
+    // console.log('outerIndex =  ', outerIndex);
+    // console.log('counter =  ', this.counter);
+    // console.log(this.currentTransition);
   }
 
   public dec(): void {
-    // const outerIndex = (this.counter - 1) % this.total;
-    // this.counter = (this.counter > 0 ) ? this.counter - 1 : this.total - 1;
-    // // this.amount = (this.amount > this.counter) ? this.counter - 2 : this.counter - 1;
-    //
-    // for (let i = 0; i < this.total; i++) {
-    //   this.currentTransition[i] = this.currentTransition[i] + 500;
-    // }
-    //
-    // console.log(this.counter - 1 < 0 ? this.total - 1 : this.counter);
-    // this.currentTransition[outerIndex] = this.currentTransition[outerIndex] + 500 * this.total;
-    // this.currentTransition[this.counter - 1 < 0 ? this.total - 1 : this.counter] -= 500 * this.total;
-    // console.log('% ' + this.counter % this.total);
-    // console.log('count ' + this.counter);
-    // console.log('amount ' + this.amount);
+    this.counter = (this.counter > 0 ) ? this.counter - 1 : this.total - 1;
+    let outerIndex = this.counter - 1;
+
+    outerIndex = (outerIndex < 0) ? this.total - 1 : outerIndex;
+
+    for (let i = 0; i < this.total; i++) {
+      this.currentTransition[i] = this.currentTransition[i] + 500;
+    }
+
+    this.currentTransition[outerIndex] = this.currentTransition[outerIndex] - 500 * this.total;
+    // console.log('count = ' + this.counter);
+    // console.log('outerIndex = ' + outerIndex);
+    // console.log(this.currentTransition);
   }
 
 
