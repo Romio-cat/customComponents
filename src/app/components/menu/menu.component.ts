1d6c7a39283ac1f3062149500fdaaea134d7760c
Romio-cat <kaluzhenov95@gmail.com>
2019-05-12 21:17:48 +0300

    few changes

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  public menuItems: string[] = ['procedures', 'slider', 'multiselect'];
 
   constructor() { }
 
